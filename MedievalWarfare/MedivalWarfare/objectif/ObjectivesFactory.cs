e79b5f19e2ec6ed3b1058e9fe222c7ed0b0905c9
FuglyLionKing <gudel.aubry@gmail.fr>
2014-01-17 02:53:11 +0100

    Objectives factory

---
+++ MedievalWarfare/MedivalWarfare/objectif/ObjectivesFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using GHBesignPattern.Model.Characters;
+using GHBesignPattern.Model.Items;
+
+namespace MedievalWarfare.MedivalWarfare.objectif
+{
+    public class ObjectivesFactory
+    {
+        public static ObjectiveTester PickUpObjectif(IItem item)
+        {
+            return
+                charac => charac.Items.Contains(item);
+        }
+
+
+        public static List<Objectif> GenerateObjectivesFrom(List<IItem> items, int desiredNumber)
+        {
+            var ans = new List<Objectif>();
+
+            for (; 0 < desiredNumber; desiredNumber--)
+            {
+                IItem item = items[desiredNumber%items.Count];
+                var obj = new Objectif(PickUpObjectif(item)) {target = item};
+            }
+
+
+            return ans;
+        }
+    }
+}
